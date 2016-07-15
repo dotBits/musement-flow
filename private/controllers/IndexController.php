60c289562fc7009b18aa6e5f372dfe2d96f425e2
Cristian Ronzio <cristian.ronzio@musement.com>
2016-07-15 16:50:25 +0200

    first modification from repo-2

--- private/controllers/IndexController.php
+++ private/controllers/IndexController.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: devel
- * Date: 15/07/16
- * Time: 15:47
- */
+
+class IndexController
+{
+
+    private $member;
+
+    public function __construct()
+    {
+        $this->member = 'test';
+    }
+
+}
